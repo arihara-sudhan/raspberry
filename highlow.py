41cbb8b29fd3a497abb5a75835f6f565a7defaa7
Southern Boy <100225369+arihara-sudhan@users.noreply.github.com>
2025-04-11 23:17:46 -0700

    Update highlow.py

--- highlow.py
+++ highlow.py
@@ -5,10 +5,10 @@ PIN = int(input("PIN: "))
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(PIN, GPIO.OUT)
 
-print("Turning ON pin 11")
+print(f"Turning ON PIN {PIN}")
 GPIO.output(PIN, GPIO.HIGH)
 time.sleep(10)
 
-print("Turning OFF pin 11")
+print(f"Turning OFF PIN {PIN}")
 GPIO.output(PIN, GPIO.LOW)
 GPIO.cleanup()
