346b6aae010e5d537a7e99e9768f80dbda02f660
Southern Boy <100225369+arihara-sudhan@users.noreply.github.com>
2025-04-11 22:37:23 -0700

    Update highlow.py

--- highlow.py
+++ highlow.py
@@ -6,16 +6,8 @@ LED_PIN = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED_PIN, GPIO.OUT)
 
-try:
-    while True:
-        # Turn the LED on
-        GPIO.output(LED_PIN, GPIO.HIGH)
-        time.sleep(1)  # Wait for 1 second
-
-        # Turn the LED off
-        GPIO.output(LED_PIN, GPIO.LOW)
-        time.sleep(1)  # Wait for 1 second
-
-except KeyboardInterrupt:
-    # Clean up the GPIO pins
-    GPIO.cleanup()
+GPIO.output(LED_PIN, GPIO.HIGH)
+time.sleep(1)  # Wait for 1 second
+GPIO.output(LED_PIN, GPIO.LOW)
+time.sleep(1)  # Wait for 1 second
+GPIO.cleanup()
