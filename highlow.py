a097ae147a49fde5a8221e7988bc30c1c749a794
Southern Boy <100225369+arihara-sudhan@users.noreply.github.com>
2025-04-12 00:59:01 -0700

    Update highlow.py

--- highlow.py
+++ highlow.py
@@ -4,18 +4,7 @@ import time
 PIN = 7
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(PIN, GPIO.OUT)
-
-print("ON")
 GPIO.output(PIN, GPIO.HIGH)
-time.sleep(3)
-print("OFF")
-GPIO.output(PIN, GPIO.LOW)
-time.sleep(3)
-print("ON")
-GPIO.output(PIN, GPIO.HIGH)
-time.sleep(3)
-print("OFF")
-GPIO.output(PIN, GPIO.LOW)
-time.sleep(3)
+time.sleep(30)
 
 GPIO.cleanup()
