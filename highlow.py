6a89486083be0e4b3a35e944897149c60d22b57c
Southern Boy <100225369+arihara-sudhan@users.noreply.github.com>
2025-04-11 23:33:35 -0700

    Update highlow.py

--- highlow.py
+++ highlow.py
@@ -1,17 +1,14 @@
 import RPi.GPIO as GPIO
 import time
 
-PIN = int(input("PIN: "))
-try:
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(PIN, GPIO.OUT)
-    print(f"Turning ON PIN {PIN}")
+PIN = 7
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(PIN, GPIO.OUT)
+
+for i in range(100):
     GPIO.output(PIN, GPIO.HIGH)
-    time.sleep(10)
-    print(f"Turning OFF PIN {PIN}")
+    time.sleep(1)
     GPIO.output(PIN, GPIO.LOW)
-except KeyboardInterrupt:
-    GPIO.cleanup()
-except Exception as e:
-    print(f"An error occurred: {e}")
-    GPIO.cleanup()
+    time.sleep(1)
+
+GPIO.cleanup()
