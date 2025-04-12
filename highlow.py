0bd92dd5b28a8c540d725b612943ce5c0253664f
Southern Boy <100225369+arihara-sudhan@users.noreply.github.com>
2025-04-11 22:56:41 -0700

    Update highlow.py

--- highlow.py
+++ highlow.py
@@ -1,18 +1,25 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
+# Use physical pin numbering
+GPIO.setmode(GPIO.BOARD)
+
+# Use physical pin 11 (which is GPIO 17 on the Pi)
 led_pin = 11
+
+# Set the pin as an output
 GPIO.setup(led_pin, GPIO.OUT)
 
-print("Starting LED blink loop...")
+print("Starting LED blink loop using BOARD mode...")
 
 try:
     while True:
+        # Turn LED ON
         GPIO.output(led_pin, GPIO.HIGH)
         print("LED ON")
         time.sleep(5)
 
+        # Turn LED OFF
         GPIO.output(led_pin, GPIO.LOW)
         print("LED OFF")
         time.sleep(5)
