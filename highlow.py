b3a82495e30b7bccbc4d43ba66cbe0aeb58cc72a
Southern Boy <100225369+arihara-sudhan@users.noreply.github.com>
2025-04-11 22:44:37 -0700

    Update highlow.py

--- highlow.py
+++ highlow.py
@@ -1,14 +1,19 @@
 import RPi.GPIO as GPIO
 import time
+
 # Set up the GPIO pin for the on-board LED
-LED_PIN = 47  # The on-board LED is connected to GPIO47 on the Raspberry Pi
+LED_PIN = 35  # The on-board LED is connected to GPIO35 on the Raspberry Pi Model 2 B
 
 # Set the GPIO mode and configure the LED pin as an output
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(LED_PIN, GPIO.OUT)
 
 # Turn the LED off
 GPIO.output(LED_PIN, GPIO.LOW)
+
+# Wait for 10 seconds
+print("Turning off the LED for 10 seconds...")
 time.sleep(10)
+
 # Clean up the GPIO pins
 GPIO.cleanup()
